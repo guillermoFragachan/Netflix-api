333c7983d773d39a991ecb5b67abed38aa17a2c1
guillermoFragachan <guillefragachan@gmail.com>
2021-09-24 15:53:16 +0200

    put mehotd working

--- js/code.js
+++ js/code.js
@@ -82,7 +82,17 @@ function getData(_gnre='comedy') {
 		.then((data) => {
 			console.log(data)
 
+			if(idURL){
+				console.log(data)
+
+
+			}else{
+
 			createCarousel(data)
+
+
+			}
+			
 
 
 			
@@ -110,6 +120,7 @@ function createCarousel(_data, _row='trendingNow' ){
 		//send to movie info
 		IMGs[i].onclick=()=>{
 			window.location.assign(`./movieDetail.html?id=`+_data[i]._id);
+			//+`&?category=`+_data[i].category
 
 		}
 	
@@ -120,6 +131,84 @@ function createCarousel(_data, _row='trendingNow' ){
 
 
 }
+
+
+//url params
+const idURL = new URLSearchParams(location.search).get("id")
+const categoryURL =  new URLSearchParams(window.location.search).get("category")
+
+
+
+//function to delete data
+function deleteData(_id){
+	fetch("https://striveschool-api.herokuapp.com/api/movies/"+_id, {
+method: 'DELETE',
+
+headers: {
+
+	"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWM5NDRiYjUzZDAwMTViMTllY2MiLCJpYXQiOjE2MzI0NzY2NDAsImV4cCI6MTYzMzY4NjI0MH0.EfADlnD4qaCkTQXgxZufji8eoBYtPLM0N5yAamp7gCU",
+	'Content-Type': 'application/json'
+}
+}).then(window.location.assign("/")
+
+)
+
+}
+
+
+
+
+
+
+
+
+
+//suppossed to bring data to the backoffice DOESNT WORK YET
+
+function bringDatatoDetailPage(_urlID=idURL){
+	fetch("https://striveschool-api.herokuapp.com/api/movies/"+_id, {
+method: 'GET',
+
+headers: {
+
+	"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWM5NDRiYjUzZDAwMTViMTllY2MiLCJpYXQiOjE2MzI0NzY2NDAsImV4cCI6MTYzMzY4NjI0MH0.EfADlnD4qaCkTQXgxZufji8eoBYtPLM0N5yAamp7gCU",
+	'Content-Type': 'application/json'
+}
+}).then()
+
+
+
+
+}
+
+
+let obj = {
+				name: 'dsadas',
+                description:'dsadadsa' ,
+                category: 'romance',
+                imageUrl: 'https://m.media-amazon.com/images/M/MV5BMzUzMmRlYTAtZjJkYy00YWM2LWJhNzUtOGZkOGJjNWFiY2NkXkEyXkFqcGdeQXVyMzM1MjQzNTk@._V1_.jpg'
+
+}
+
+//edit function
+function editData(_url, obj) {
+	fetch("https://striveschool-api.herokuapp.com/api/movies/"+_url, {
+		method: 'PUT',
+		body: JSON.stringify(obj),
+		headers: {
+
+			"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMWM5NDRiYjUzZDAwMTViMTllY2MiLCJpYXQiOjE2MzI0NzY2NDAsImV4cCI6MTYzMzY4NjI0MH0.EfADlnD4qaCkTQXgxZufji8eoBYtPLM0N5yAamp7gCU",
+			'Content-Type': 'application/json'
+		}
+	})
+
+}
+
+
+function submit(){
+
+}
+
 
 
 
